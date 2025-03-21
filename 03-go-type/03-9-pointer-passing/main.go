671b5d4bf4c53450a063ca332383700cc9350a3e
Somprasong Damyos <somprasong.damyos@gmail.com>
2025-03-21 11:52:29 +0700

    update exmaple code

--- 03-go-type/03-9-pointer-passing/main.go
+++ 03-go-type/03-9-pointer-passing/main.go
@@ -4,20 +4,28 @@ import (
 	"fmt"
 )
 
-func inc(n int) {
-	n = n + 1
+func changeValue(x int) {
+	x = 100 // เปลี่ยนค่า x ภายในฟังก์ชัน
+	fmt.Println("ค่าภายในฟังก์ชัน:", x)
 }
 
-func incPointer(n *int) {
-	*n = *n + 1
+func changeValuePointer(x *int) {
+	*x = 100 // ใช้ pointer เพื่อเปลี่ยนค่าของตัวแปรต้นฉบับ
+	fmt.Println("ค่าภายในฟังก์ชัน [pointer]:", *x)
 }
 
 func main() {
-	i := 1
+	a := 10
 
-	inc(i)
-	fmt.Println(i) // ผลลัพท์: 1
+	changeValue(a)
+	fmt.Println("ค่าภายนอกฟังก์ชัน:", a) // ค่า a ไม่เปลี่ยนแปลง
 
-	incPointer(&i)
-	fmt.Println(i) // ผลลัพท์: 2
+	changeValuePointer(&a)                         // ส่ง pointer ของ a ไป
+	fmt.Println("ค่าภายนอกฟังก์ชัน [pointer]:", a) // ค่า a ถูกเปลี่ยนแปลง
 }
+
+// ผลลัพธ์:
+// ค่าภายในฟังก์ชัน: 100
+// ค่าภายนอกฟังก์ชัน: 10
+// ค่าภายในฟังก์ชัน [pointer]: 100
+// ค่าภายนอกฟังก์ชัน [pointer]: 100
